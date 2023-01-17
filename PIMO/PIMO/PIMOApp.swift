caa8d97032916b0f50bdb4c00388a68df561cdc8
okstring <milkoi7801@gmail.com>
2023-01-17 16:26:51 +0900

    init

---
+++ PIMO/PIMO/PIMOApp.swift
@@ -0,0 +1,17 @@
+//
+//  PIMOApp.swift
+//  PIMO
+//
+//  Created by Ok Hyeon Kim on 2023/01/17.
+//
+
+import SwiftUI
+
+@main
+struct PIMOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
