a5893ec5a94c28821b848948b1e49246a422f22e
okstring <milkoi7801@gmail.com>
2023-01-27 16:53:47 +0900

    feature #7: UserClient, UserUtill 구현, 기존 User 클래스 삭제

---
+++ PIMO/Sources/App/Client/UserClient.swift
@@ -0,0 +1,28 @@
+//
+//  UserClient.swift
+//  PIMO
+//
+//  Created by Ok Hyeon Kim on 2023/01/27.
+//  Copyright © 2023 pimo. All rights reserved.
+//
+
+import SwiftUI
+
+import Combine
+import ComposableArchitecture
+
+struct UserClient {
+    let getToken: () -> MemberToken?
+    let setToken: (_ token: MemberToken) -> Void
+}
+
+extension UserClient {
+    static let live: Self = .init(
+        getToken: {
+            UserUtill.shared.getToken()
+        },
+        setToken: { token in
+            UserUtill.shared.setUserDefaults(key: UserDefaultsKeys.token, value: token)
+        }
+    )
+}
