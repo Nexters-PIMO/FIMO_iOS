71db1e0cdb7d182b8add6a5562de12d7f9a72995
yanghojoon <yanghojoon8487@gmail.com>
2023-02-08 22:31:43 +0900

    chore #8: 카카오 SDK 추가

--- Project.swift
+++ Project.swift
@@ -11,4 +11,5 @@ let project = Project.app(
         .external(name: "Alamofire"),
         .external(name: "ComposableArchitecture"),
         .external(name: "Kingfisher")
+        .external(name: "KakaoSDK")
     ])
