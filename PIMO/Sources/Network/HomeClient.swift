af5d51d133c12c2f7db60e25be7cc41f2456eb72
김영인 <0inn1220@gmail.com>
2023-02-21 01:53:44 +0900

    feat #17: 홈 피드 화면 구현

--- PIMO/Sources/Network/HomeClient.swift
+++ PIMO/Sources/Network/HomeClient.swift
@@ -26,14 +26,17 @@ extension HomeClient: DependencyKey {
     static let liveValue = Self.init (
         fetchFeeds: {
             // TODO: 서버 통신
-            return [Feed(profile: Profile(imageURL: "", nickName: "0inn1"),
-                         uploadTime: "10:00",
-                         textImages: [TextImage(imageURL: "", text: "안녕")],
+            return [Feed(id: 1,
+                         profile: Profile(imageURL: PIMOStrings.profileImage, nickName: "0inn1"),
+                         uploadTime: "5분 전",
+                         textImages: [TextImage(id: 1, imageURL: PIMOStrings.textImage, text: "안녕"),
+                                      TextImage(id: 2, imageURL: PIMOStrings.textImage, text: "안녕")],
                          clapCount: 310,
                          isClapped: false),
-                    Feed(profile: Profile(imageURL: "", nickName: "0inn2"),
-                         uploadTime: "09:00",
-                         textImages: [TextImage(imageURL: "", text: "안녕")],
+                    Feed(id: 2,
+                         profile: Profile(imageURL: PIMOStrings.profileImage, nickName: "0inn2"),
+                         uploadTime: "10분 전",
+                         textImages: [TextImage(id: 2, imageURL: PIMOStrings.textImage, text: "안녕")],
                          clapCount: 320,
                          isClapped: true)]
         }
--- PIMO/Sources/View/Home/HomeView.swift
+++ PIMO/Sources/View/Home/HomeView.swift
@@ -22,14 +22,33 @@ struct HomeView: View {
                 if viewStore.feeds.isEmpty {
                     homeWelcome
                 } else {
-                    FeedView()
-                        .frame(maxHeight: .infinity)
+                    homeFeedView(viewStore: viewStore)
                 }
             }
             .onAppear {
                 viewStore.send(.fetchFeeds)
             }
         }
+    }
+    
+    func homeFeedView(viewStore: ViewStore<HomeStore.State, HomeStore.Action>) -> some View {
+        ScrollView {
+            LazyVStack(alignment: .center) {
+                VStack {
+                    ForEach(viewStore.feeds, id: \.id) { feed in
+                        FeedView(feed: feed)
+                        
+                        Spacer()
+                            .frame(height: 12)
+                        
+                        Divider()
+                            .background(Color(PIMOAsset.Assets.grayDivider.color))
+                            .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+                    }
+                }
+            }
+        }
+        .scrollIndicators(.hidden)
     }
 }
 
