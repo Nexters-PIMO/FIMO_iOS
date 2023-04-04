0961b8b8aca0765a712a0a9a364e4c200948aaf6
김영인 <0inn1220@gmail.com>
2023-04-04 15:44:32 +0900

    fix #38: 카카오톡 앱 로그인 안되는 이슈 수정
    앱 돌아왔을 때 로그인 처리하는 방식 SwiftUI로 수정

--- PIMO/Sources/App/AppDelegate.swift
+++ PIMO/Sources/App/AppDelegate.swift
@@ -32,18 +32,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(
-        _ app: UIApplication,
-        open url: URL,
-        options: [UIApplication.OpenURLOptionsKey : Any] = [:]
-    ) -> Bool {
-        if (AuthApi.isKakaoTalkLoginUrl(url)) {
-            return AuthController.handleOpenUrl(url: url)
-        }
-
-        return false
-    }
-    
-    func application(
         _ application: UIApplication,
         configurationForConnecting connectingSceneSession: UISceneSession,
         options: UIScene.ConnectionOptions
